808465bf1e6d56acba684d24463824583a35f83b
devin <devin@mailfence.com>
2026-03-10 10:31:56 -0300

    implement read progress update function

--- read-later.el
+++ read-later.el
@@ -196,17 +196,21 @@
   (interactive)
   (let((id (tabulated-list-get-id)))
     (message (format "Updating read progress for: %S" id))
-    (read-later--update-bookmark-read-progress id 0))))
+    (read-later--update-bookmark-read-progress id 0)))
 
 (defun read-later--update-bookmark-read-progress (id progress)
   "Update the bookmarks with ID to read PROGRESS."
   (read-later-api-full-request 'bookmarks-update-progress
-                               :params `(("bookmark_id" . ,(number-to-string id)) ("progress" . ,(number-to-string progress)) ("progress_timestamp" . ,(number-to-string (floor (float-time)))))
+                               :params `(("bookmark_id" . ,(number-to-string id))
+                                         ("progress" . ,(number-to-string progress))
+                                         ("progress_timestamp" . ,(number-to-string (floor (float-time)))))
                                :callback (lambda (result)
                                            (message (format "RESULT: %S" result))
-                                           (let ((success (plist-get result :success)))
+                                           (let ((success (plist-get result :success))
+                                                 (bookmarks (read-later--handle-request-body result :type "bookmark")))
                                              (if success
                                                  (progn
+                                                   (read-later--update-bookmark (car bookmarks))
                                                    (message "Bookmark read progress updated"))
                                                (message "Bookmark read progress update failed"))))))
 
@@ -236,18 +240,21 @@
         (append read-later--bookmarks-data bookmarks))
   (read-later--display-bookmarks read-later--bookmarks-data))
 
-(defun read-later--remove-bookmarks (bookmarks)
-  "Remove BOOKMARKS list."
+(defun read-later--remove-bookmarks (bookmark_ids)
+  "Remove BOOKMARK_IDS list."
   (setq read-later--bookmarks-data
-        (cl-remove-if (lambda (b) (member (plist-get b :bookmark_id) bookmarks))
+        (cl-remove-if (lambda (b) (member (plist-get b :bookmark_id) bookmark_ids))
                       read-later--bookmarks-data))
   (read-later--display-bookmarks read-later--bookmarks-data))
 
 (defun read-later--update-bookmark (bookmark)
   "Update BOOKMARK fields inside tabulated list. The bookmark must have an ID."
-  ;;get the bookmark record from the tabulated list
-  ;;update the bookmark with the provided bookmark
-  )
+  (interactive)
+  (let ((bookmark_id (plist-get bookmark :bookmark_id)))
+    (progn
+      (read-later--remove-bookmarks (list bookmark_id))
+      (setq read-later--bookmarks-data (cons bookmark read-later--bookmarks-data))
+      (read-later--display-bookmarks read-later--bookmarks-data))))
 
 ;; ========================================= UTILITY FUNCTIONS =========================================
 
